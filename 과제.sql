16d4422805eeab6bf1d767b9a685f4f37c374af3
HyunChulKang <godapple123123@gmail.com>
2019-12-14 15:34:00 +0900

    SQL-HR CREATE 과제

--- 과제.sql
+++ 과제.sql
@@ -1,4 +1,4 @@
-CREATE TABLE jobs ( --**
+CREATE TABLE jobs ( 
 		job_id VARCHAR2(10),
 		job_title VARCHAR2(35),
 		min_salary NUMBER(6),
@@ -6,13 +6,12 @@ CREATE TABLE jobs ( --**
         CONSTRAINT pk_job_id PRIMARY KEY (job_id)
 );
 
-CREATE TABLE regions ( --**
+CREATE TABLE regions ( 
 		region_id NUMBER,
 		region_name VARCHAR2(25),
         CONSTRAINT pk_region_id PRIMARY KEY (region_id)
 );
-
-CREATE TABLE countries ( --**
+CREATE TABLE countries ( 
 		country_id CHAR(2),
 		country_name VARCHAR2(40),
 		region_id NUMBER,
@@ -20,7 +19,7 @@ CREATE TABLE countries ( --**
 );
 		ALTER TABLE countries ADD CONSTRAINT fk_region_id FOREIGN KEY (region_id) REFERENCES regions (region_id);
 
-CREATE TABLE locations ( --**
+CREATE TABLE locations ( 
 		location_id NUMBER(4),
 		street_address VARCHAR2(40),
 		postal_code VARCHAR2(12),
@@ -29,22 +28,27 @@ CREATE TABLE locations ( --**
 		country_id CHAR(2),
         CONSTRAINT pk_location_id PRIMARY KEY (location_id)
 );
-		ALTER TABLE locations ADD CONSTRAINT fk_cnt_id FOREIGN KEY (fk_country_id) REFERENCES countries (country_id);
+		ALTER TABLE locations ADD CONSTRAINT fk_cnt_id FOREIGN KEY (country_id) REFERENCES countries (country_id);
 
 CREATE TABLE departments ( 
 		department_id NUMBER(4),
 		department_name VARCHAR2(30),
+        manager_id NUMBER(6),
+        location_id NUMBER(4),
         CONSTRAINT pk_department_id PRIMARY KEY (department_id)
 );
-        ALTER TABLE departments ADD CONSTRAINT fk_loc_id FOREIGN KEY (fk_location_id) REFERENCES locations (location_id);
+        ALTER TABLE departments ADD CONSTRAINT fk_loc_id FOREIGN KEY (location_id) REFERENCES locations (location_id);
         
 CREATE TABLE job_history (
+        employee_id NUMBER(6),
 		start_date DATE ,
 		end_date DATE,
-        CONSTRAINT pk_start_date PRIMARY KEY (start_date)
+        job_id VARCHAR2(10),
+        department_id NUMBER(4),
+        CONSTRAINT pk_start_date PRIMARY KEY (start_date,employee_id)
 );
         ALTER TABLE job_history ADD CONSTRAINT fk_job_id FOREIGN KEY (job_id) REFERENCES jobs (job_id);
-        ALTER TABLE job_history ADD CONSTRAINT fk_dept_id FOREIGN KEY (department_id) REFERENCES departments(department_id);
+        ALTER TABLE job_history ADD CONSTRAINT fk_deptment_id FOREIGN KEY (department_id) REFERENCES departments(department_id);
 CREATE TABLE employees  (
 		employee_id NUMBER(6),
 		first_name VARCHAR2(20),
@@ -52,19 +56,22 @@ CREATE TABLE employees  (
 		email VARCHAR2(25),
 		phone_number VARCHAR2(20),
 		hire_date DATE,
+        job_id VARCHAR2(10),
 		salary NUMBER (8,2),
 		commission_pct NUMBER(2,2),
+        manager_id NUMBER(6),
+        department_id NUMBER(4),
         CONSTRAINT pk_employee_id PRIMARY KEY (employee_id)
 );
         ALTER TABLE employees ADD CONSTRAINT fk_dept_id FOREIGN KEY (department_id) REFERENCES departments (department_id);
-        ALTER TABLE departments ADD CONSTRAINT fk_mgrager_id FOREIGN KEY (manager_id) REFERENCES employees (employee_id);
+        ALTER TABLE employees ADD CONSTRAINT fk_mgrager_id FOREIGN KEY (manager_id) REFERENCES employees (employee_id);
 		ALTER TABLE employees ADD CONSTRAINT fk_jobs_id FOREIGN KEY (job_id) REFERENCES jobs (job_id);
-        ALTER TABLE job_history ADD CONSTRAINT fk_emp_id FOREIGN KEY (EMPLOYEE_ID) REFERENCES employees (EMPLOYEE_ID);
+        
        
 		
         
 		
-		
+commit;	
 
 
 
@@ -76,3 +83,5 @@ DROP TABLE 	departments;
 DROP TABLE 	job_history;
 DROP TABLE 	jobs;
 DROP TABLE 	employees;
+
+commit;	
